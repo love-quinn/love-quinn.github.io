608ea12e5f4dd6b4e44cd73aad5d3ca14ff27d60
love-quinn <lucaslucal75@gmail.com>
2025-05-31 12:31:36 -0300

    fix: import createInstance plausible

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import { createInstance } from "plausible-tracker";
 // Initialize Plausible
 try {
   const plausible = createInstance({
